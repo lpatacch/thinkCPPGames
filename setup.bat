53f38b722186e409ba45ae7f066fe9d5e4a95a98
Lisa Patacchiola <lpatacch@gmail.com>
2023-03-15 00:39:20 -0400

    improved the creation tool
    moved the location of the html to where the pages will be shared

--- setup.bat
+++ setup.bat
@@ -1,4 +1,7 @@
-set PATH=%PATH%;D:\Users\Lisa\AppData\Local\Programs\MiKTeX\miktex\bin\x64\;c:\Users\Lisa\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\Scripts"\
+set PATH=%PATH%;D:%HOMEPATH%\AppData\Local\Programs\MiKTeX\miktex\bin\x64\;c:%HOMEPATH%\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\Scripts"\
 cd book
 plastex html.tex --split-level=1
 py ..\fixhtml\fixhtml.py
+cd ..
+rmdir /Q /S docs
+move book\html docs
