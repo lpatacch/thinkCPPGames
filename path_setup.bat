d3c9d08f2b93c55ffe9f9f1c4e551e464137caec
Lisa Patacchiola <lpatacch@gmail.com>
2023-03-20 08:36:19 -0400

    started on Function chapter
    1. Added a light green color to code listings
    2. Updated my webpage tools to have a once setup and a continued create
    3. Started modifying chapter 5. Added std:: to some code sections. Explained why using namespace is bad. Fixed some references and mentions to math.h instead of cmath. Added a section talking about pre-made math values. I'll probably want a const/constexpr section, but I will do that after functions are over.

---
+++ path_setup.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;D:%HOMEPATH%\AppData\Local\Programs\MiKTeX\miktex\bin\x64\;c:%HOMEPATH%\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\Scripts"\
+setup.bat
--- setup.bat
+++ setup.bat
@@ -1,4 +1,3 @@
-set PATH=%PATH%;D:%HOMEPATH%\AppData\Local\Programs\MiKTeX\miktex\bin\x64\;c:%HOMEPATH%\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\Scripts"\
 cd book
 plastex html.tex --split-level=1
 py ..\fixhtml\fixhtml.py
